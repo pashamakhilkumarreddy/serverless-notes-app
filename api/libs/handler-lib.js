64031eab4337441cc6e88ae4a69d098ced4a57ec
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2021-03-22 14:25:54 +0530

    update api gateway default response type

--- api/libs/handler-lib.js
+++ api/libs/handler-lib.js
@@ -13,11 +13,11 @@ export default function handler(lambda) {
     }
     return {
       statusCode,
-      body: JSON.stringify(body),
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
+      body: JSON.stringify(body),
     };
   };
 };
