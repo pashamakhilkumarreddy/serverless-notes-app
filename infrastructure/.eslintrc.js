0f16c5c12a3f1a47f58ab6bb6e86bab9320a6ee6
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2021-03-25 03:37:19 +0530

    remove unnecessary packages

--- infrastructure/.eslintrc.js
+++ infrastructure/.eslintrc.js
@@ -7,15 +7,10 @@ module.exports = {
   extends: [
     'airbnb-base',
   ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
   rules: {
-    'no-new': 0,
   },
 };
