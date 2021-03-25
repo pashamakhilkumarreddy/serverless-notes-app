7b409a3accfab855cfdba945c1fc851b51c0d62c
pashamakhilkumarreddy <pashamakhilkumarreddy@gmail.com>
2021-03-26 00:19:55 +0530

    update serverless packages

--- infrastructure/.eslintrc.js
+++ infrastructure/.eslintrc.js
@@ -7,10 +7,15 @@ module.exports = {
   extends: [
     'airbnb-base',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
+  plugins: [
+    '@typescript-eslint',
+  ],
   rules: {
+    'no-new': 0,
   },
 };
